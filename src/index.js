78c9282ce7ff3adf1e35daec4f61cf53cad0b320
Umair Majeed <umair.majeed@globalfundsolutions.com>
2024-05-20 17:10:55 +0500

    publish new package with 1.0.4

--- src/index.js
+++ src/index.js
@@ -21,6 +21,55 @@ async function getTemplateHtml(htmlTemplatePath) {
 async function generatePDF(htmlTemplatePath, data = {}, isPublish = false, pdfFormatDetails = {}) {
   let PDF;
   await getTemplateHtml(htmlTemplatePath).then(async (res) => {
+
+    hb.registerHelper('ifCond', function (v1, operator, v2, options) {
+      switch (operator) {
+        case '==':
+          return (v1 == v2) ? options.fn(this) : options.inverse(this);
+        case '===':
+          return (v1 === v2) ? options.fn(this) : options.inverse(this);
+        case '!=':
+          return (v1 != v2) ? options.fn(this) : options.inverse(this);
+        case '!==':
+          return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+        case '<':
+          return (v1 < v2) ? options.fn(this) : options.inverse(this);
+        case '<=':
+          return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+        case '>':
+          return (v1 > v2) ? options.fn(this) : options.inverse(this);
+        case '>=':
+          return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+        case '&&':
+          return (v1 && v2) ? options.fn(this) : options.inverse(this);
+        case '||':
+          return (v1 || v2) ? options.fn(this) : options.inverse(this);
+        default:
+          return options.inverse(this);
+      }
+    });
+
+    hb.registerHelper('Operate', function (v1, operator, v2) {
+      switch (operator) {
+        case '+':
+          return Number(v1) + Number(v2);
+        case '-':
+          return Number(v1) - Number(v2);
+        case '*':
+          return Number(v1) * Number(v2);
+        case '%':
+          return Number(v1) % Number(v2);
+        case '/':
+          return Number(v1) / Number(v2);
+        default:
+          return "";
+      }
+    });
+
+    hb.registerHelper("setVar", function (varName, varValue, options) {
+      options.data.root[varName] = varValue;
+    });
+
     // Now we have the html code of our template in res object
     // you can check by logging it on console
     const template = hb.compile(res);
