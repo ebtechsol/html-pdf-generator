22217c378f06e613ff0b7f5613eb895664059e00
Umair Majeed <umair.majeed@globalfundsolutions.com>
2024-06-03 20:02:15 +0500

    implement s3 bucket template path with new version 1.0.5

--- src/index.js
+++ src/index.js
@@ -5,13 +5,24 @@ const puppeteer = require("puppeteer");
 const hb = require("handlebars");
 const readFile = utils.promisify(fs.readFile);
 const chromium = require('@sparticuz/chromium');
+const AWS = require("aws-sdk")
+const s3 = new AWS.S3();
 
 // Loading HTML template
-async function getTemplateHtml(htmlTemplatePath) {
+async function getTemplateHtml(htmlTemplatePath, isPublish) {
   let templatePath;
   try {
-    templatePath = path.resolve(htmlTemplatePath);
-    return await readFile(templatePath, "utf8");
+    if(isPublish == true) { // s3 bucket
+      const S3URLArray = htmlTemplatePath.split('//')[1];
+      const bucketName = S3URLArray.split('/', 1)[0];
+      const keyName = S3URLArray.split('/').slice(1).join('/');
+      var params = { Bucket : bucketName, Key : keyName }
+      const s3Object = await s3.getObject(params).promise();
+      return s3Object.Body.toString();
+    } else {
+      templatePath = path.resolve(htmlTemplatePath);
+      return await readFile(templatePath, "utf8");
+    }
   } catch (err) {
     return Promise.reject("Could not load html template");
   }
@@ -20,7 +31,7 @@ async function getTemplateHtml(htmlTemplatePath) {
 // Generating PDF
 async function generatePDF(htmlTemplatePath, data = {}, isPublish = false, pdfFormatDetails = {}) {
   let PDF;
-  await getTemplateHtml(htmlTemplatePath).then(async (res) => {
+  await getTemplateHtml(htmlTemplatePath, isPublish).then(async (res) => {
 
     hb.registerHelper('ifCond', function (v1, operator, v2, options) {
       switch (operator) {
